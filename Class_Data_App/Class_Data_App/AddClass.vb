201769c041303ad34a1fb3851cb18db6b66c36b4
Andrew Shepherd <andrewshepherd5396@gmail.com>
2018-01-30 15:57:34 -0600

    add constraint to class add

--- Class_Data_App/Class_Data_App/AddClass.vb
+++ Class_Data_App/Class_Data_App/AddClass.vb
@@ -1,4 +1,5 @@
-﻿Public Class AddClass
+﻿Imports System.Data.SqlClient
+Public Class AddClass
     Protected db As New db
     Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
         Me.Dispose()
@@ -6,7 +7,16 @@
     End Sub
 
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
-        If IsNumeric(txtTid.Text) = True Then
+        Dim connection As New SqlConnection With {.ConnectionString = "Server=essql1.walton.uark.edu;Database=arshephe; Trusted_Connection=yes"}
+        Dim command As New SqlCommand("Select * FROM Instructor where TID = @tid", connection)
+        command.Parameters.Add("@tid", SqlDbType.SmallInt).Value = txtTid.Text
+
+        Dim adapter As New SqlDataAdapter(command)
+        Dim table As New DataTable()
+        adapter.Fill(table)
+        If table.Rows.Count() <= 0 Then
+            MsgBox("This instructor ID is not present in the Instructors table")
+        ElseIf IsNumeric(txtTid.Text) = True Then
             db.sql = "INSERT INTO CLASS VALUES (@name, @time, @tid, @room);"
             db.bind("@name", txtName.Text)
             db.bind("@time", txtTime.Text)
@@ -16,6 +26,7 @@
             Me.Dispose()
         Else
             MsgBox("Instructor ID must be numeric")
+
         End If
     End Sub
 End Class
